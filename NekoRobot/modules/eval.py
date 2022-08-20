3276379b71ca3493a3b0512bfab4e7156301f73d
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-08-20 08:40:44 +0530

    Reverted evel

--- NekoRobot/modules/eval.py
+++ NekoRobot/modules/eval.py
@@ -1,57 +1,41 @@
 """
-BSD 2-Clause License
-
+MIT License
 Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
-Copyright (c) 2022-2023, BlackLover • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Copyright (C) 2022 Awesome-Prince
+Copyright (c) 2022, Koyūki • Network, <https://github.com/Awesome-Prince/NekoRobot>
+This file is part of @NekoXRobot (Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import ast
 import io
 import os
-import sys
+
+# Common imports for eval
 import textwrap
 import traceback
+
 from contextlib import redirect_stdout
-from inspect import getfullargspec
-from io import StringIO
-from time import time
-
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram import ParseMode, Update
-from telegram.ext import CallbackContext
-
-from NekoRobot import DEV_USERS, LOGGER
-from NekoRobot import pbot as pgram
+from telegram.ext import CallbackContext, CommandHandler
+from NekoRobot import LOGGER, dispatcher
 from NekoRobot.modules.helper_funcs.chat_status import dev_plus
 
-Neko_PYRO_Eval = filters.command(["eval", "e"])
 namespaces = {}
-
 
 def namespace_of(chat, update, bot):
     if chat not in namespaces:
@@ -65,56 +49,43 @@ def namespace_of(chat, update, bot):
         }
     return namespaces[chat]
 
-
 def log_input(update):
     user = update.effective_user.id
     chat = update.effective_chat.id
     LOGGER.info(f"IN: {update.effective_message.text} (user={user}, chat={chat})")
-
-
-async def send(msg, bot, update):
+    
+def send(msg, bot, update):
     if len(str(msg)) > 2000:
         with io.BytesIO(str.encode(msg)) as out_file:
             out_file.name = "output.txt"
-            await bot.send_document(chat_id=update.effective_chat.id, document=out_file)
+            bot.send_document(chat_id=update.effective_chat.id, document=out_file)
     else:
         LOGGER.info(f"OUT: '{msg}'")
-        await bot.send_message(
+        bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f"`{msg}`",
-            parse_mode=ParseMode.MARKDOWN,
+            text=f"<b>Result</b>:\n<code>{msg}</code>",
+            parse_mode=ParseMode.HTML,
         )
 
 
-async def aexec(code, client, message):
-    exec(
-        "async def __aexec(client, message): "
-        + "".join(f"\n {a}" for a in code.split("\n"))
-    )
-    return await locals()["__aexec"](client, message)
-
-
-async def edit_or_reply(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    await func(**{k: v for k, v in kwargs.items() if k in spec})
-
-
 @dev_plus
-async def execute(update: Update, context: CallbackContext) -> None:
+def evaluate(update: Update, context: CallbackContext):
     bot = context.bot
-    await send((await do(exec, bot, update)), bot, update)
-
-
+    send(do(eval, bot, update), bot, update)
+    
+@dev_plus
+def execute(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(exec, bot, update), bot, update)
+    
 def cleanup_code(code):
     if code.startswith("```") and code.endswith("```"):
         return "\n".join(code.split("\n")[1:-1])
     return code.strip("` \n")
 
-
-async def do(func, bot, update):
+def do(func, bot, update):
     log_input(update)
-    content = await update.message.text.split(" ", 1)[-1]
+    content = update.message.text.split(" ", 1)[-1]
     body = cleanup_code(content)
     env = namespace_of(update.message.chat_id, update, bot)
     os.chdir(os.getcwd())
@@ -133,7 +104,7 @@ async def do(func, bot, update):
     try:
         with redirect_stdout(stdout):
             func_return = func()
-    except Exception:
+    except Exception as e:
         value = stdout.getvalue()
         return f"{value}{traceback.format_exc()}"
     else:
@@ -152,79 +123,29 @@ async def do(func, bot, update):
         if result:
             return result
 
-
-@pgram.on_message(
-    Neko_PYRO_Eval & filters.user(DEV_USERS) & (~filters.forwarded) & (~filters.via_bot)
+@dev_plus
+def clear(update: Update, context: CallbackContext):
+    bot = context.bot
+    log_input(update)
+    global namespaces
+    if update.message.chat_id in namespaces:
+        del namespaces[update.message.chat_id]
+    send("Cleared locals.", bot, update)
+    
+EVAL_HANDLER = CommandHandler(
+    ("e", "ev", "eva", "eval"),
+    evaluate,
+    run_async=True,
 )
-@pgram.on_edited_message(Neko_PYRO_Eval)
-async def executor(client, message):
-    try:
-        cmd = message.text.split(" ", maxsplit=1)[1]
-    except IndexError:
-        return await message.delete()
-    t1 = time()
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = StringIO()
-    redirected_error = sys.stderr = StringIO()
-    stdout, stderr, exc = None, None, None
-    try:
-        await aexec(cmd, client, message)
-    except Exception:
-        exc = traceback.format_exc()
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
-    final_output = f"**OUTPUT**:\n```{evaluation.strip()}```"
-    if len(final_output) > 4096:
-        filename = "output.txt"
-        with open(filename, "w+", encoding="utf8") as out_file:
-            out_file.write(str(evaluation.strip()))
-        t2 = time()
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="⏳",
-                        callback_data=f"runtime {t2-t1} Seconds",
-                    )
-                ]
-            ]
-        )
-        await message.reply_document(
-            document=filename,
-            caption=f"**INPUT:**\n`{cmd[:980]}`\n\n**OUTPUT:**\n`Attached Document`",
-            quote=False,
-            reply_markup=keyboard,
-        )
-        await message.delete()
-        os.remove(filename)
-    else:
-        t2 = time()
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="⏳",
-                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
-                    )
-                ]
-            ]
-        )
-        await edit_or_reply(message, text=final_output, reply_markup=keyboard)
+EXEC_HANDLER = CommandHandler(
+    ("x", "ex", "exe", "exec", "py"),
+    execute,
+    run_async=True,
+)
+CLEAR_HANDLER = CommandHandler("clearlocals", clear, run_async=True)
 
+dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(EXEC_HANDLER)
+dispatcher.add_handler(CLEAR_HANDLER)
 
-@pgram.on_callback_query(filters.regex(r"runtime"))
-async def runtime_func_cq(_, cq):
-    runtime = cq.data.split(None, 1)[1]
-    await cq.answer(runtime, show_alert=True)
+__mod_name__ = "Eval"
